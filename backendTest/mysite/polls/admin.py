062fe1ce4d7e32bd3a6e0c55062339ae2e1667a3
MAtterholt <matterholt84@gmail.com>
2019-09-29 11:45:04 -0400

    registering model in admin panel

--- backendTest/mysite/polls/admin.py
+++ backendTest/mysite/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# register models
+from .models import Question, Choice
 
-# Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choice)
