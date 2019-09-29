e6ec608ae9d3a419db5e9556baa5e43d2f596b5a
MAtterholt <matterholt84@gmail.com>
2019-09-29 11:17:31 -0400

    working with models

--- backendTest/mysite/polls/models.py
+++ backendTest/mysite/polls/models.py
@@ -1,15 +1,23 @@
+import datetime
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
 
 
 class Question(models.Model):
+    def was_published_recently(self):
+        return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
     question_text = models.CharField(max_length=200)
     pub_data = models.DateTimeField('date published')
-    counter_name = models.CharField(max_length=200, default="v00")
 
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
