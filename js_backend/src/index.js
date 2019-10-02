3d2f680b3fc4dc0ad48b48b9bf17e9389623ba0e
MAtterholt <matterholt84@gmail.com>
2019-10-01 23:25:06 -0400

    initiate server

--- js_backend/src/index.js
+++ js_backend/src/index.js
@@ -1,5 +1,12 @@
-import 'dotenv/config';
+import "dotenv/config";
+import express from "express";
 
-console.log('Hello ever running Node.js project');
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () =>
+  console.log("Example app listening on port http://localhost:3000/")
+);
